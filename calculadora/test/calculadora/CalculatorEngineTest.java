e9440c2bc9bac605a03ac404c4a9f4880ec2b6dd
mknarciso <murilokn@gmail.com>
2016-06-22 10:53:27 -0300

    Second test: add()

--- calculadora/test/calculadora/CalculatorEngineTest.java
+++ calculadora/test/calculadora/CalculatorEngineTest.java
@@ -7,20 +7,24 @@ import org.junit.Test;
 public class CalculatorEngineTest {
 
 	@Test
-	public void testDigit() {
-		System.out.println("Testing Digit input:\n Will input 5, then will input 7, expect 57\n");
+	public void testDigit() throws Exception {
+		System.out.println("Testing Digit input:\n Will input 5, then will input 7, expect 57");
 		CalculatorEngine engine = new CalculatorEngine();
 		engine.digit(5);
-		System.out.println(engine.display());
-		assertEquals(5,engine.display());
+		engine.add();
 		engine.digit(7);
-		System.out.println(engine.display());
-		assertEquals(57,engine.display());
+		engine.compute();
+		assertEquals(12,engine.display());
 	}
 	
 	@Test
 	public void testAdd() {
-		fail("Not yet implemented");
+		System.out.println("Testing Add operation:\n Will input 5, then add with 7, expect 12.");
+		CalculatorEngine engine = new CalculatorEngine();
+		engine.digit(5);
+		assertEquals(5,engine.display());
+		engine.digit(7);
+		assertEquals(57,engine.display());
 	}
 
 	@Test
