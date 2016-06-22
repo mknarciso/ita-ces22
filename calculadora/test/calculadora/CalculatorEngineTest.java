14c8df374bcffafd72f80cc6f896fe733d91744c
mknarciso <murilokn@gmail.com>
2016-06-22 11:05:18 -0300

    Exception test: Divide by zero

--- calculadora/test/calculadora/CalculatorEngineTest.java
+++ calculadora/test/calculadora/CalculatorEngineTest.java
@@ -5,10 +5,30 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class CalculatorEngineTest {
+	
+	@Test
+	public void testDigit() {
+		System.out.println("Testing Digit input: Will input 5, then will input 7, expect 57");
+		CalculatorEngine engine = new CalculatorEngine();
+		engine.digit(5);
+		assertEquals(5,engine.display());
+		engine.digit(7);
+		assertEquals(57,engine.display());
+	}
+	
+	@Test
+	public void testClear() {
+		System.out.println("Testing Clear operation: Will input 5, then clear, expect 0.");
+		CalculatorEngine engine = new CalculatorEngine();
+		engine.digit(5);
+		assertEquals(5,engine.display());
+		engine.clear();
+		assertEquals(0,engine.display());
+	}
 
 	@Test
-	public void testDigit() throws Exception {
-		System.out.println("Testing Digit input:\n Will input 5, then will input 7, expect 57");
+	public void testAdd() throws Exception {
+		System.out.println("Testing Add operation: Will input 5, then add with 7, expect 12.");
 		CalculatorEngine engine = new CalculatorEngine();
 		engine.digit(5);
 		engine.add();
@@ -16,30 +36,52 @@ public class CalculatorEngineTest {
 		engine.compute();
 		assertEquals(12,engine.display());
 	}
-	
+
+
 	@Test
-	public void testAdd() {
-		System.out.println("Testing Add operation:\n Will input 5, then add with 7, expect 12.");
+	public void testSubtract() throws Exception  {
+		System.out.println("Testing Subtract operation: Will input 7, then subtract 5, expect 2.");
 		CalculatorEngine engine = new CalculatorEngine();
-		engine.digit(5);
-		assertEquals(5,engine.display());
 		engine.digit(7);
-		assertEquals(57,engine.display());
+		assertEquals(7,engine.display());
+		engine.subtract();
+		engine.digit(5);
+		engine.compute();
+		assertEquals(2,engine.display());
 	}
 
 	@Test
-	public void testSubtract() {
-		fail("Not yet implemented");
+	public void testMultiply() throws Exception  {
+		System.out.println("Testing Multiply operation: Will input 7, then multiply by 5, expect 35.");
+		CalculatorEngine engine = new CalculatorEngine();
+		engine.digit(7);
+		assertEquals(7,engine.display());
+		engine.multiply();
+		engine.digit(5);
+		engine.compute();
+		assertEquals(35,engine.display());
 	}
 
 	@Test
-	public void testMultiply() {
-		fail("Not yet implemented");
+	public void testDivide() throws Exception  {
+		System.out.println("Testing Divide operation: Will input 8, then divide by 4, expect 2.");
+		CalculatorEngine engine = new CalculatorEngine();
+		engine.digit(8);
+		assertEquals(8,engine.display());
+		engine.divide();
+		engine.digit(4);
+		engine.compute();
+		assertEquals(2,engine.display());
 	}
 
-	@Test
-	public void testDivide() {
-		fail("Not yet implemented");
-	}
-
+    @Test(expected=Exception.class) 
+    public void divideByZeroTest() throws Exception  {
+		System.out.println("Testing Divide by zero: Will input 8, then divide by zero, expect exception.");
+		CalculatorEngine engine = new CalculatorEngine();
+		engine.digit(8);
+		assertEquals(8,engine.display());
+		engine.divide();
+		engine.digit(0);
+		engine.compute();
+     }
 }
